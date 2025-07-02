5cac6df7a66917b59deee9817eb9e097ee8f3332
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-07-02 19:46:54 +0100

    Create script.js

---
+++ Courses/JavaScript/Beginners/The Console/05. Receipt/script.js
@@ -0,0 +1,22 @@
+// Write code below 💖
+
+console.log("Congrats! You've made it.");
+
+/*
+🍔 Burger       1          2.99   
+🍟 Fries        1          2.49   
+🥤 Soda         1          1.99  
+*/
+console.log("==================================")
+console.log("         McDONALD'S RECEIPT       ") // I did not go mcdonalds pipe down
+console.log("----------------------------------") // very long line
+console.log("           Order Details          ")
+console.log("----------------------------------")
+console.log("Item           Quantity   Price")
+console.log("----------------------------------")
+console.log("🍔 Burger       1          2.99   ")
+console.log("🍟 Fries        1          2.49 ")
+console.log("🥤 Soda         1          1.99  ")
+console.log("----------------------------------")
+console.log("Total                      7.47   ")
+console.log("----------------------------------")
