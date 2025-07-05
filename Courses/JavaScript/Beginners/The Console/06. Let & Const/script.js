d1583d77462416986b77e2d9e0a0ca87bc16e022
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-07-05 21:56:01 +0100

    Update script.js
    Forgot to change a variable

--- Courses/JavaScript/Beginners/The Console/06. Let & Const/script.js
+++ Courses/JavaScript/Beginners/The Console/06. Let & Const/script.js
@@ -7,3 +7,7 @@ let currentLocation = "home";
 let currentMood = "vibing";
 
 console.log('Hey my name is ' + firstName + ' and my favourite color is ' + favColor + ' I am currently at ' + currentLocation + ' and I am ' + currentMood + '.');
+
+currentMood = "angry";
+
+console.log('Never mind I am now ' + currentMood + ".");
