f9722276bce3efa139b8958c51405af1551a4860
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-07-05 22:05:35 +0100

    Create index.js

---
+++ Courses/JavaScript/Beginners/The Console/07. Data Types/index.js
@@ -0,0 +1,28 @@
+// Write code below 💖
+
+/* A number can be any integer or decimal number. It can be used to represent whole numbers, fractions or precise measurements.
+
+let year = 2023;
+let age = 28;
+let mealCost = 12.99;
+const pi = 3.14;
+const daysOfWeek = 7;
+
+Instructions
+Create some variables for your favorite company with the following data types:
+
+companyName, a string.
+foundingYear, a number.
+isActive, a Boolean.
+fundingAmount, undefined.
+Print them out using console.log(). */ 
+
+const companyName = "RustyLoot";
+const foundingYear = "2022";
+const isActive = true;
+const fundingAmount = undefined;
+
+console.log(`Company        : ${companyName}
+Founded in     : ${foundingYear}
+Is active      : ${isActive}
+Funding amount : ${fundingAmount}`);
