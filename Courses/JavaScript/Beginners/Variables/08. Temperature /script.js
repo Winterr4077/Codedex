7ad1b89968fbb28d0c1e0c3d75283b7dc3caa8f3
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-07-05 22:16:50 +0100

    Create script.js

---
+++ Courses/JavaScript/Beginners/Variables/08. Temperature /script.js
@@ -0,0 +1,49 @@
+// Write code below 💖
+
+/* 
+JavaScript has the following arithmetic operators:
+
++ Addition
+- Subtraction
+* Multiplication
+/ Division
+% Modulo (returns the remainder)
+** Exponent (covered in the next exercise) 
+
+The following code calculates a 20% tip by adding the total price of a pizza and coke, and then multiplying by a decimal number (also known as a float):
+
+const pizza = 2.99;
+const coke = 0.99;
+
+let total = pizza + coke;
+
+let tip = total * 0.2;
+
+console.log(tip);     Output: 0.796 
+
+Instructions
+
+Let's put these operators to use!
+
+Create a temperature.js program that converts a temperature from Fahrenheit (°F) to Celsius (°C).
+
+Google the current temperature of Brooklyn, NY (where Codédex is based) in °F.
+
+Use the following formula and write it out in JavaScript:
+
+
+Celsius=(Fahrenheit−32)
+            1.8
+​
+
+
+Print out the converted temperature using console.log().
+
+Currently : 29°C or 85°F
+*/
+
+let tempretureF = 85;
+
+tempretureC = (tempretureF - 32) / 1.8;
+
+console.log(tempretureC);
