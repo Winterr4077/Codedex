2a3f8cc74ab08c2143345c9c6f2a9fe94a724f1e
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-07-13 19:56:05 +0100

    Create script.js

---
+++ Courses/JavaScript/Beginners/Variables/10. Planet Weight/script.js
@@ -0,0 +1,7 @@
+// Write code below 💖
+
+const earthWeight = 210;
+const marsWeight = earthWeight * 0.38;
+
+console.log("Your weight on Earth is " + earthWeight + " pounds.");
+console.log("Your weight on Mars is " + marsWeight + " pounds.");
