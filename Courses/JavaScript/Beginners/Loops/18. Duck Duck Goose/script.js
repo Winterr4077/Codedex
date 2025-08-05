9cdfb11507239a4eedef36a896caf47bd43c380f
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-08-05 18:01:55 +0100

    Create script.js

---
+++ Courses/JavaScript/Beginners/Loops/18. Duck Duck Goose/script.js
@@ -0,0 +1,10 @@
+// Write code below 💖
+
+let randomNumber = Math.floor(Math.random() * 10);
+
+while (randomNumber !== 7) {
+  console.log("Duck 🦆");
+  randomNumber = Math.floor(Math.random() * 10);
+}
+
+console.log("Goose! 🦢");
