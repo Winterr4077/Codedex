ff990fbee699fbd4540e8da28c527d5c367c8d3e
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-08-16 09:54:48 +0100

    Create script.py

---
+++ Courses/Python/Beginners/Variables/06. Data Types/script.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+
+# The variable name can consist of letters, numbers, and the _ underscore.
+# These are all valid variable names and values:
+
+name = 'Erlich Bachman'
+user_id = 16180339887
+progress = 0.75
+xp = 60
+verified = True
+
+# The = equal sign means assignment:
