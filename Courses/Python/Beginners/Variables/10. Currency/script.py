ca8a12cd0f83621863cda9385289e5c9fde6bada
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-08-16 10:54:31 +0100

    Create script.py

---
+++ Courses/Python/Beginners/Variables/10. Currency/script.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+
+pesos = float(input('What do you have left in pesos?'))
+soles = float(input('What do you have left in soles?'))
+reais = float(input('What do you have left in reais?'))
+
+usd = (pesos * 0.00025) + (soles * 0.28) + (reais * 0.19)
+
+print(usd)
