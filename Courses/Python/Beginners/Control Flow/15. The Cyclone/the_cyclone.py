a9c5d6517ed66412b83236039ffadc4f49e9a66b
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-08-22 10:40:17 +0200

    Create the_cyclone.py

---
+++ Courses/Python/Beginners/Control Flow/15. The Cyclone/the_cyclone.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+
+height = float(input('What is your height (cm): '))
+credits = int(input('How many credits do you have: '))
+
+if height >= 137 and credits >= 10:
+    print('Enjoy the ride!')
+elif height < 137 and credits >= 10:
+    print('You are not tall enough to ride.')
+elif height >= 137 and credits < 10:
+    print("You don't have enough credits.")
+else: 
+    print('You have not met either requirement for this ride.')
