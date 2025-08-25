6ee75b63107a1a6699bec6f47b15712e5e20c023
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-08-25 12:10:20 +0200

    Create script.js

---
+++ Challenge Packs/Javascript/Conditionals/TGIF/script.js
@@ -0,0 +1,13 @@
+// Write code below 💖
+
+let day = 4;
+
+if (day >= 1 && day < 5){
+  console.log("Not Friday, yet!")
+} else if (day === 5){
+  console.log("TGIF 🕺")
+} else if (day === 6 || day === 7){
+  console.log("Yay, weekends! 🙌")
+} else {
+  console.log("Wait, what day is it?")
+}
