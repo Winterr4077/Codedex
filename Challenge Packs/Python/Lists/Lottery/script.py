a2820bdd74bc69473a558ce6642259a24967732e
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-08-31 12:06:43 +0100

    Create script.py

---
+++ Challenge Packs/Python/Lists/Lottery/script.py
@@ -0,0 +1,16 @@
+# Write code below 💖
+
+import random
+
+my_numbers = []
+winning_numbers = []
+
+for i in range(5):
+    my_numbers.append(random.randint(1, 69))
+    winning_numbers.append(random.randint(1, 69))
+
+my_numbers.append(random.randint(1, 26))
+winning_numbers.append(random.randint(1, 26))
+
+print("My Numbers:", my_numbers)
+print("Winning Numbers:", winning_numbers)
