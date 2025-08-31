66d6dc059f0404e9e292dd8411c05aa7f3dd9949
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-08-31 12:33:15 +0100

    Create script.py

---
+++ Challenge Packs/Python/Lists/The Oscars/script.py
@@ -0,0 +1,21 @@
+# Write code below 💖
+
+best_pictures = [
+  '2019 - Parasite',
+  '2018 - Green Book',
+  '2017 - The Shape of Water',
+  '2016 - Moonlight',
+  '2015 - Spotlight',
+  '2014 - Birdman',
+  '2013 - 12 Years a Slave',
+  '2012 - Argo',
+  '2011 - The Artist'
+]
+
+best_pictures = ['2020 - Nomadland'] + best_pictures
+best_pictures = ['2021 - CODA'] + best_pictures
+best_pictures = ['2022 - Everything Everywhere All at Once'] + best_pictures
+best_pictures = ['2023 - Oppenheimer'] + best_pictures
+best_pictures = ['2024 - Anora'] + best_pictures
+
+print(best_pictures)
