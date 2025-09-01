7a595729bf553717b448f1988d984f7ccd639822
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-01 09:21:28 +0100

    Create script.js

---
+++ Courses/JavaScript/Beginners/Loops/21. Even The Odds/script.js
@@ -0,0 +1,12 @@
+// Write code below 💖
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 === 1) {
+    continue;
+  } else if (i === 42) {
+    console.log(i);
+    break;
+  } else {
+    console.log(i);
+  }
+}
