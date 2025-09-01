e4f18cd37b52afa5aac4793d7c7fb733724611fe
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-01 09:37:40 +0100

    Create query.sql

---
+++ Courses/SQL/Beginners/Queries/02. SELECT/query.sql
@@ -0,0 +1,7 @@
+-- Write code below 💖
+
+-- SELECT * FROM shows; 
+-- This would select all collumns from the table name shows.
+
+SELECT name, genre FROM shows;
+-- This would select the collumns name and genre from the table name shows.
