31b59bf154f287dffd720a92d9d991149bed4e52
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-01 09:54:35 +0100

    Create query.sql

---
+++ Courses/SQL/Beginners/Queries/04. Rotten Tomatoes/query.sql
@@ -0,0 +1,7 @@
+-- Write code below 💖
+
+-- SELECT * FROM shows WHERE tomatometer > 60 
+-- A red tomato is displayed to indicate it's "Fresh". 🍅
+
+SELECT * FROM shows WHERE tomatometer < 60
+-- A green splat is displayed to indicate it's "Rotten". 🦠
