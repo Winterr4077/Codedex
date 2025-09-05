1b0d749d495ba31b153914115b9f60e3dad4fd0a
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-05 20:35:05 +0100

    Create index.js

---
+++ Courses/JavaScript/Beginners/Arrays/25. Times Tables/index.js
@@ -0,0 +1,9 @@
+// Write code below 💖
+
+let multiple = 2;
+let numbers = 9;
+
+for (let i = 0; i <= numbers; i++) {
+  let calculation = i * multiple; 
+  console.log(`${multiple} x ${i} = ${calculation}`);
+}
