4cf2d45a69a607b9a76e9495249508245346d337
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-06 11:10:28 +0100

    Create index.js

---
+++ Courses/JavaScript/Beginners/Arrays/26. Music Playlist/index.js
@@ -0,0 +1,22 @@
+// Write code below 💖
+
+let musicPlaylist = [
+  "Tom Sawyer",
+  "Sabotage",
+  "I Wanna Dance With Somebody",
+  "Don't Speak",
+  "Bulls On Parade",
+  "Thriller",
+  "The Breaks",
+  "Brick",
+  "Aeroplane Over the Sea",
+  "Tubthumping"
+];
+
+musicPlaylist.shift();
+musicPlaylist.pop();
+
+musicPlaylist[0] = "Music 1";
+musicPlaylist[1] = "Music 2";
+
+console.log(musicPlaylist)
