0cb5ed357db33226b4afa19dfd8502dc28c8c0e9
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-12 12:42:39 +0100

    Create script.js

---
+++ Courses/JavaScript/Beginners/Functions/31. E = mc²/script.js
@@ -0,0 +1,10 @@
+// Write code below 💖
+
+function relativityTheory(mass) {
+  const speedOfLight = 3e8; 
+  let energy = mass * (speedOfLight ** 2);
+
+  return energy;
+}
+
+console.log(relativityTheory(5));
