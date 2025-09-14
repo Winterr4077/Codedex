47b08c8a2f22f45c0d8a2a04dbaa5b778c899334
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-14 14:25:05 +0100

    Create script.js

---
+++ Courses/JavaScript/Beginners/Objects/35. Dream Car/script.js
@@ -0,0 +1,14 @@
+// Write code below 💖
+
+const car = {
+  model: "Tesla",
+  year: "2003",
+  color: 'Red',
+  used: false
+ };
+
+if (car.used) {
+  console.log("I want a " + car.color + " " + car.year + " " + car.model + " that is used.");
+} else {
+  console.log("I want a " + car.color + " " + car.year + " " + car.model + " that is new.");
+}
