1513363d1a58bd1e4834862c84cfb8acd1b529a7
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-14 14:28:56 +0100

    Create script.js

---
+++ Courses/JavaScript/Beginners/Objects/36. Pokémon/script.js
@@ -0,0 +1,18 @@
+// Write code below 💖
+
+pokemon = {
+  name: "Pikachu",
+  type: "Electric ⚡️",
+  level: 25,
+}
+
+console.log(pokemon)
+console.log('-----------------------------')
+
+pokemon.isCaught = false;
+console.log(pokemon.isCaught)
+console.log('-----------------------------')
+
+pokemon.isCaught = true;
+pokemon.name = 'Charizard';
+console.log(pokemon)
