a9fe52202cc6b5ea061dce60da6f1c5d1dfa638f
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-22 16:59:34 +0100

    Create main.cpp

---
+++ Courses/C++/Beginners/Hello World/05. Sign Bunny/main.cpp
@@ -0,0 +1,13 @@
+// Write code below 💖
+
+#include <iostream>
+
+int main() {
+  std::cout << "|￣￣￣￣￣￣￣|\n";
+  std::cout << "|   YOU MADE  |\n";
+  std::cout << "|     IT!     |\n";
+  std::cout << "|＿＿＿＿＿＿＿|\n";
+  std::cout << "(\\__/) ||     \n";
+  std::cout << "(•ㅅ•) ||      \n";
+  std::cout << "/ 　 づ         \n";
+}
