bf47c0b9b98c6f759cda47ac7c3545c44744e386
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-22 17:06:37 +0100

    Create main.cpp

---
+++ Courses/C++/Beginners/Variables/06. Data Types/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+// Int
+int year = 2024;
+int age = 28;
+
+// Double
+double pi = 3.14159;
+double meal_cost = 12.99;
+
+// Char
+char mention = '@';
+char hashtag = '#';
+
+// String
+std::string message = "good nite 😊";
+std::string username = "@potus";
+
+// Boolean
+
+bool late_to_class = false;
+bool cranky = true;
+}
