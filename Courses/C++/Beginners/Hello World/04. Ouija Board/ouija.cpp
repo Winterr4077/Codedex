fc6ba7c6a48fad0d03971efa00e1e54db4dfe67b
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-22 16:53:40 +0100

    Create ouija.cpp

---
+++ Courses/C++/Beginners/Hello World/04. Ouija Board/ouija.cpp
@@ -0,0 +1,20 @@
+// Write code below 💖
+
+/* This is commented out.
+std::cout << "Oof! I'm commented out";
+Yep! This is one, too.
+std::cout << "Noooo, please help!";
+*/
+
+#include <iostream> 
+
+int main() {
+  std::cout << "-----------------------------------\n";
+  std::cout << "|          x YES   NO  x          |\n";
+  std::cout << "|                                 |\n";
+  std::cout << "|         ABCDEFGHIJKLM           |\n";
+  std::cout << "|         NOPQRSTUVWXYZ           |\n";
+  std::cout << "|                                 |\n";
+  std::cout << "|           1234567890            |\n";
+  std::cout << "-----------------------------------\n";
+}
