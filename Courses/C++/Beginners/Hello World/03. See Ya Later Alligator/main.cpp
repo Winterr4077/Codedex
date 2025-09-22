60bbd4835034c39a0cb0c48ff7f555565e963474
Winterr4077 <97064959+Winterr4077@users.noreply.github.com>
2025-09-22 16:49:10 +0100

    Create main.cpp

---
+++ Courses/C++/Beginners/Hello World/03. See Ya Later Alligator/main.cpp
@@ -0,0 +1,11 @@
+// Write code below 💖
+
+#include <iostream> 
+
+int main() {
+  std::cout << "Goodbye."; 
+  std::cout << "Auf Wiedersehen."; 
+  std::cout << "再見."; 
+  std::cout << "Αντίο."; 
+  std::cout << "Прощайте."; 
+}
